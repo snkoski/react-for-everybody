65ce2c5deaa854bc09387743d6322929c15cef93
snkoski <shawnnkoski@gmail.com>
2018-09-11 12:47:50 -0400

    Install prop-types. Examples of defaultProps and PropTypes in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import {hot} from 'react-hot-loader';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1> Hello World!</h1>
+        <h1> Hello {this.props.name}</h1>
       </div>
     )
   }
+
+}
+
+// Setting default props in ES6
+App.defaultProps = {
+    name: "Me"
+}
+
+// Making sure props are the right type ES6
+App.propTypes = {
+  name: PropTypes.string
 }
 
 export default hot(module)(App);
